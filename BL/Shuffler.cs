b32ee6f9b757de26b64bb55ac605ef45204fee02
AntonN <000job00@gmail.com>
2015-04-01 00:16:39 +0300

    Add shuffler & test for it

---
+++ BL/Shuffler.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BL
+{
+    public interface IShuffler
+    {
+    }
+
+    public class Shuffler : IShuffler
+    {
+        public static Random random = new Random();
+        public List<RecordEntity> ShuffleItems(List<RecordEntity> records)
+        {
+            return records.OrderBy( x => ( random.Next() ) ).ToList();
+        } 
+    }
+}
---
+++ EnglishTranslate/Tests/ShufflerTest.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using ApprovalTests;
+using BL;
+using FluentAssertions;
+using Newtonsoft.Json;
+using Xunit;
+
+namespace Tests
+{
+    public class ShufflerTest
+    {
+        [Fact]
+        public void should_shuffle_items()
+        {
+            var arrangeItems = new List<RecordEntity>
+            {
+                new RecordEntity
+                {
+                    EnglishWord = "first",
+                    RussianWord = "first"
+                },
+                new RecordEntity
+                {
+                    EnglishWord = "second",
+                    RussianWord = "second"
+                },
+                new RecordEntity
+                {
+                    EnglishWord = "third",
+                    RussianWord = "third"
+                },
+                new RecordEntity
+                {
+                    EnglishWord = "fourth",
+                    RussianWord = "fourth"
+                },
+
+            };
+            Shuffler shuffler = new Shuffler();
+            var result1 = shuffler.ShuffleItems(arrangeItems);
+            var result2 = shuffler.ShuffleItems( arrangeItems);
+            result1[0].Should().NotBeSameAs(result2[0]);
+        } 
+    }
+}
