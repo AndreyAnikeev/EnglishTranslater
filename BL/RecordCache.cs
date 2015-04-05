3cdc39145998ca6e4d9959e931d6d5bce1392045
AntonN <000job00@gmail.com>
2015-04-05 23:22:03 +0300

    Add logic for RecorCache

--- BL/RecordCache.cs
+++ BL/RecordCache.cs
@@ -1,11 +1,14 @@
-﻿namespace BL
+﻿using System.Collections.Generic;
+
+namespace BL
 {
     public interface IRecordCache
     {
+        List<RecordEntity> RecordList { get; set; }
     }
 
     public class RecordCache : IRecordCache
     {
-         
+        public List<RecordEntity> RecordList { get; set; }
     }
 }
