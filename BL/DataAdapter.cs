702238389bdea537b63df316be9a15de352d8058
AntonN <000job00@gmail.com>
2015-04-05 23:28:00 +0300

    Add caching in Adapter

--- BL/DataAdapter.cs
+++ BL/DataAdapter.cs
@@ -16,17 +16,21 @@ namespace BL
     {
         private readonly IDataConverter _dataConverter;
         private readonly IDataReader _dataReader;
+        private readonly IRecordCache _cache;
 
-        public DataRecordAdapter(IDataConverter dataConverter, IDataReader dataReader)
+        public DataRecordAdapter(IDataConverter dataConverter, IDataReader dataReader, IRecordCache cache)
         {
             _dataConverter = dataConverter;
             _dataReader = dataReader;
+            _cache = cache;
         }
 
         public List<RecordEntity> GetRecords(string path)
         {
             var data = _dataReader.ReadDataFromFile(path);
-            return _dataConverter.ConvertDataToRecords( data );
+            var records = _dataConverter.ConvertDataToRecords( data );
+            _cache.RecordList = records;
+            return records;
         } 
     }
 }
