16fb76a85db5bd800f862b359d93468c46207d46
AntonN <000job00@gmail.com>
2015-04-05 22:39:06 +0300

    Add interface for shuffler

--- BL/Shuffler.cs
+++ BL/Shuffler.cs
@@ -6,11 +6,16 @@ namespace BL
 {
     public interface IShuffler
     {
+        List<RecordEntity> ShuffleItems(List<RecordEntity> records);
     }
 
     public class Shuffler : IShuffler
     {
+        /// <summary>
+        /// This parameter is static for getting unrepeatable result.
+        /// </summary>
         public static Random random = new Random();
+
         public List<RecordEntity> ShuffleItems(List<RecordEntity> records)
         {
             return records.OrderBy( x => ( random.Next() ) ).ToList();
